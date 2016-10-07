104d1cf340deb7f418c6c667f7eaeedeb62d2b2b
MDS Dev <productions@mediadesignschool.com>
2016-10-07 17:07:01 +1300

    Alembic Publish MDS Edition 1.0
    - Updated path locations
    - New icons installed
    - Implemented new tagger
    - Configured publisher to pick up new tags
    - Generate cameras, alembics and scene files both in asset and shot
    context

---
+++ config/hooks/revilo_scan_scene_tk-maya.py
@@ -0,0 +1,109 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+import maya.cmds as cmds
+
+import tank
+from tank import Hook
+from tank import TankError
+
+class ScanSceneHook(Hook):
+    """
+    Hook to scan scene for items to publish
+    """
+    
+    def execute(self, **kwargs):
+        """
+        Main hook entry point
+        :returns:       A list of any items that were found to be published.  
+                        Each item in the list should be a dictionary containing 
+                        the following keys:
+                        {
+                            type:   String
+                                    This should match a scene_item_type defined in
+                                    one of the outputs in the configuration and is 
+                                    used to determine the outputs that should be 
+                                    published for the item
+                                    
+                            name:   String
+                                    Name to use for the item in the UI
+                            
+                            description:    String
+                                            Description of the item to use in the UI
+                                            
+                            selected:       Bool
+                                            Initial selected state of item in the UI.  
+                                            Items are selected by default.
+                                            
+                            required:       Bool
+                                            Required state of item in the UI.  If True then
+                                            item will not be deselectable.  Items are not
+                                            required by default.
+                                            
+                            other_params:   Dictionary
+                                            Optional dictionary that will be passed to the
+                                            pre-publish and publish hooks
+                        }
+        """   
+                
+        items = []
+        
+        # get the main scene:
+        scene_name = cmds.file(query=True, sn=True)
+        if not scene_name:
+            raise TankError("Please Save your file before Publishing")
+        
+        scene_path = os.path.abspath(scene_name)
+        name = os.path.basename(scene_path)
+
+        # create the primary item - this will match the primary output 'scene_item_type':            
+        items.append({"type": "work_file", "name": name})
+
+        # Revilo - look for cameras to publish based on MDS metadata:
+        for camera in cmds.listCameras(perspective=True):
+            if cmds.attributeQuery('mdsMetadata', n=camera, exists=True):
+
+                # Evaluate shutterAngle Settings
+                subFrame = cmds.getAttr(camera + ".subframe")
+
+                if subFrame == 0:
+                    shutterAngle = 0.25
+                if subFrame == 1:
+                    shutterAngle = 0.2
+                if subFrame == 2:
+                    shutterAngle = 0.125
+
+                items.append({"type": "camera", "name": camera, "shutterAngle": shutterAngle})
+
+        # Revilo - look for geo to publish based on MDS metadata:
+        for geo in cmds.ls(assemblies=True, long=True):
+            if cmds.ls(geo, dag=True, type="mesh"):
+
+                # Geo found - does it have a tag?
+                if cmds.attributeQuery('mdsMetadata', n=geo, exists=True):
+
+                    # Tag found - Let's define a name for geo called <Asset/Shot>_<Step>
+                    geoName = geo.split("_group")[0].strip("|")
+
+                    # Evaluate shutterAngle attributes
+                    subFrame = cmds.getAttr(geo + ".subframe")
+
+                    if subFrame == 0:
+                        shutterAngle = 0.25
+                    if subFrame == 1:
+                        shutterAngle = 0.2
+                    if subFrame == 2:
+                        shutterAngle = 0.125
+
+                    # include this group as a 'mesh_group', record the selection, the name and the shutterAngle
+                    items.append({"type": "mesh_group", "selection" : geo, "name": geoName, "shutterAngle": shutterAngle})
+
+        return items
